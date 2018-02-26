5b608158c6cfd188a7d42a703d28f0d1b6e2b033
Stefan Schroedl <stefan.schroedl@gmx.de>
2018-02-26 09:45:02 -0800

    Copy some notebooks.

--- scripts/utils.py
+++ scripts/utils.py
@@ -29,7 +29,7 @@ def check_encoding():
     input_path = '../input/train'
     masks = [f for f in os.listdir(input_path) if f.endswith('_mask.tif')]
     masks = sorted(masks, key=lambda s: int(s.split('_')[0])*1000 + int(s.split('_')[1]))
-    
+
     encodings = []
     N = 100     # process first N masks
     for i,m in enumerate(masks[:N]):
@@ -38,12 +38,12 @@ def check_encoding():
         x = np.array(img.getdata(), dtype=np.uint8).reshape(img.size[::-1])
         x = x // 255
         encodings.append(rle_encoding(x))
-        
+
     #check output
     conv = lambda l: ' '.join(map(str, l)) # list -> string
     subject, img = 1, 1
     print('\n{},{},{}'.format(subject, img, conv(encodings[0])))
-    
+
     # train_masks.csv:
     print('1,1,168153 9 168570 15 168984 22 169401 26 169818 30 170236 34 170654 36 171072 39 171489 42 171907 44 172325 46 172742 50 173159 53 173578 54 173997 55 174416 56 174834 58 175252 60 175670 62 176088 64 176507 65 176926 66 177345 66 177764 67 178183 67 178601 69 179020 70 179438 71 179857 71 180276 71 180694 73 181113 73 181532 73 181945 2 181950 75 182365 79 182785 79 183205 78 183625 78 184045 77 184465 76 184885 75 185305 75 185725 74 186145 73 186565 72 186985 71 187405 71 187825 70 188245 69 188665 68 189085 68 189506 66 189926 65 190346 63 190766 63 191186 62 191606 62 192026 61 192446 60 192866 59 193286 59 193706 58 194126 57 194546 56 194966 55 195387 53 195807 53 196227 51 196647 50 197067 50 197487 48 197907 47 198328 45 198749 42 199169 40 199589 39 200010 35 200431 33 200853 29 201274 27 201697 20 202120 15 202544 6')
 
@@ -66,16 +66,13 @@ def precision_at(threshold, iou):
     tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
     return tp, fp, fn
 
+def iou_metric(y_true_in, y_pred_in, print_table=False):
+    labels = y_true_in
+    y_pred = y_pred_in
 
-def mean_average_precision(y_pred, labels):
-
-    # Compute number of objects
     true_objects = len(np.unique(labels))
     pred_objects = len(np.unique(y_pred))
-    print("Number of true objects:", true_objects)
-    print("Number of predicted objects:", pred_objects)
-    
-    # Compute intersection between all objects
+
     intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0]
 
     # Compute areas (needed for finding the union between all objects)
@@ -97,15 +94,22 @@ def mean_average_precision(y_pred, labels):
 
     # Loop over IoU thresholds
     prec = []
-    print("Thresh\tTP\tFP\tFN\tPrec.")
+    if print_table:
+        print("Thresh\tTP\tFP\tFN\tPrec.")
     for t in np.arange(0.5, 1.0, 0.05):
         tp, fp, fn = precision_at(t, iou)
-        p = 1.0 * tp / (tp + fp + fn)
-        print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tp, fp, fn, p))
+        if (tp + fp + fn) > 0:
+            p = tp / (tp + fp + fn)
+        else:
+            p = 0
+        if print_table:
+            print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tp, fp, fn, p))
         prec.append(p)
-    res = np.mean(prec)
-    print("AP\t-\t-\t-\t{:1.3f}".format(res))
-    return res
+
+    if print_table:
+        print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+    return np.mean(prec)
+
 
 if __name__ == '__main__':
     check_encoding()
