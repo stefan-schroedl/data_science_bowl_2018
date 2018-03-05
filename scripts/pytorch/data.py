3e39f901f8fb1264c03b96b7c6f86d6ede6fb46e
Stefan Schroedl <stefan@atomwise.com>
2018-03-04 21:05:56 -0800

    Update dataset class.

--- scripts/pytorch/data.py
+++ scripts/pytorch/data.py
@@ -1,49 +1,19 @@
 #!/usr/bin/env python
 
-from __future__ import division
 import os
 import torch
 import pandas as pd
 from skimage import io, transform
 import numpy as np
-import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
-# Ignore warnings
-import warnings
-warnings.filterwarnings("ignore")
-
-from os.path import join
-import cv2
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from glob import glob
-import os
 import skimage
 from skimage.io import imread
 import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set(color_codes=True)
-%matplotlib inline
-from skimage.color import rgb2grey, rgb2hsv, hsv2rgb, grey2rgb
-from skimage import img_as_float, img_as_ubyte
-from skimage.morphology import reconstruction
-from skimage import img_as_float, exposure
-from skimage.util import invert
-from scipy import ndimage as ndi
-from skimage.morphology import watershed
-from skimage.feature import peak_local_max
-from skimage.color import label2rgb
-from skimage.filters import gaussian
 
-from skimage.morphology import erosion, dilation, binary_dilation, opening, closing, white_tophat
-from skimage.morphology import disk
-
-from sklearn.model_selection import train_test_split
-from skimage.morphology import label
-
-ass NucleusDataset(Dataset):
+class NucleusDataset(Dataset):
     """Nucleus dataset."""
 
     @staticmethod
@@ -52,7 +22,7 @@ ass NucleusDataset(Dataset):
 
     @staticmethod
     def read_image(in_img_list):
-        return io.imread(in_img_list[0])
+        return torch.from_numpy(io.imread(in_img_list[0]))
 
     def __init__(self, root_dir, stage_name='stage1', group_name='train', transform=None):
         """
@@ -63,8 +33,9 @@ ass NucleusDataset(Dataset):
         """
         self.root_dir = root_dir
         self.transform = transform
-        
-        all_images = self.path = glob(os.path.join(dsb_data_dir, 'stage1_*', '*', '*', '*'))
+
+        all_images = glob(os.path.join(dsb_data_dir, stage_name + '_*', '*', '*', '*'))
+      
         img_df = pd.DataFrame({'path': all_images})
         img_id = lambda in_path: in_path.split('/')[-3]
         img_type = lambda in_path: in_path.split('/')[-2]
@@ -75,7 +46,7 @@ ass NucleusDataset(Dataset):
         img_df['group'] = img_df['path'].map(img_group)
         img_df['stage'] = img_df['path'].map(img_stage)
         self.img_df = img_df
-       
+
         data_df = img_df.query('group=="%s"' % group_name)
         data_rows = []
         group_cols = ['stage', 'id']
@@ -85,10 +56,7 @@ ass NucleusDataset(Dataset):
             c_row['images'] = n_rows.query('type == "images"')['path'].values.tolist()
             data_rows += [c_row]
        
-        data_df = pd.DataFrame(data_rows)  
-        data_df.head()
-        #return
-    
+        data_df = pd.DataFrame(data_rows)
         data_df['images'] = data_df['images'].map(self.read_image)
         data_df['masks'] = data_df['masks'].map(self.read_and_stack).map(lambda x: x.astype(int))
         
@@ -99,9 +67,10 @@ ass NucleusDataset(Dataset):
 
     def __getitem__(self, idx):
         
-        sample = self.data_df.iloc[idx].to_dict()
+        sample = self.data_df["images"].iloc[idx]
+        masks = self.data_df["masks"].iloc[idx]
+        
         if self.transform:
             sample = self.transform(sample)
 
-        return sample
-
+        return sample, masks
