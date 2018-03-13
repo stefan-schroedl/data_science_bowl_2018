208cdac2bc5662f30e00b2df09ac9799dc60b834
misko <misko@cs.toronto.edu>
2018-03-12 20:31:50 -0700

    added in hist match

--- scripts/KNN.py
+++ scripts/KNN.py
@@ -4,16 +4,20 @@ from sklearn.feature_extraction.image import extract_patches_2d
 from sklearn.feature_extraction.image import reconstruct_from_patches_2d
 import cv2
 import faiss   
-
+from transform import random_rotate90_transform1
 class KNN():
-    def __init__(self,n=5,patch_size=13,sample=100,gauss_blur=False,similarity=False,normalize=True):
-        self.n=5
+    def __init__(self,n=5,patch_size=13,sample=200,gauss_blur=False,similarity=False,normalize=True):
+        self.n=5 # nearest patches to average
+        self.nn=20 # nearest images to use as training
+        self.cutoff=1e5
         self.channels=8
         self.patch_size=patch_size
         self.model =  KNeighborsClassifier(n_neighbors=n,n_jobs=-1,algorithm='kd_tree') 
         self.sample = sample
         self.patches = [] #np.array([]) 
         self.patches_3d = [] #np.array([])
+        self.histograms = []
+        self.images = []
 	self.normalize = normalize
 	gkernel=cv2.getGaussianKernel(ksize=patch_size,sigma=1)
 	gkernel=gkernel*gkernel.T
@@ -23,13 +27,28 @@ class KNN():
         self.gkernel=gkernel
         self.similarity=similarity
         self.gauss_blur=gauss_blur
-        self.faiss=True
+        self.hist_match=True
+        self.img_sig=set([])
 
-    def prepare_fit(self,img,mask,mask_seg):
-	img = (img.numpy()[0].transpose(1,2,0)*255).astype(np.uint8)
-        mask = (mask.numpy()[0].transpose(1,2,0)).astype(np.uint8)
-        mask_seg = (mask_seg.numpy()[0].transpose(1,2,0)*255).astype(np.uint8)
+    def color_check(self,img):
+        if img.std(axis=2).mean()<0.01:
+            #this is black and white?
+            return False
+        return True
 
+    def get_hist(self,img):
+        color = self.color_check(img)
+        h=[[],[],[]]
+        for x in range(3):
+            h[x]=cv2.calcHist([img],[x],None,[256],[0,256])
+            h[x]/=h[x].sum()
+            h[x]*=255
+        if not color:
+            h[1]*=0+255
+            h[2]*=0
+        return np.concatenate(h)[:,0]
+
+    def get_stacked(self,img,mask,mask_seg):
         super_boundary = mask.copy()[:,:,0]*0
         super_boundary_2 = mask.copy()[:,:,0]*0
         max_components=mask.max()
@@ -43,72 +62,114 @@ class KNN():
             super_boundary_2 += boundary_thresh/255
         #print "X",super_boundary_2.max()
         super_boundary_2 = (super_boundary_2>1)*255
-        #print super_boundary_2.sum(),super_boundary_2.max()
-        #super_boundary_2 = np.minimum(super_boundary_2,3)
-        #super_boundary_2 *= 85
-        #cv2.imshow('super_boundary',super_boundary_2.astype(np.uint8))
-        #cv2.imshow('mask',mask)
-        #cv2.imshow('seg',mask_seg)
-        #cv2.waitKey(1000)
-        #img=img.numpy()[0]
-
-        #convert mask_seg to mask
-        #TODO!!!
-        #get laplacian for all way boundary, for each mask laplace, then merge?
-        #mask_seg=(mask_seg.numpy()[0].copy()*255).astype(np.uint8)
-        #mask=mask.numpy()[0]
-        #mask=np.minimum(mask,1)
-        #mask*=255
-        #boundary = cv2.Laplacian(mask_seg,cv2.CV_64F,ksize=3)
         boundary = cv2.Laplacian(mask_seg,cv2.CV_8U,ksize=3)
         boundary = boundary.reshape(boundary.shape[0],boundary.shape[1],1)
         assert(boundary.max()<=255)
         stacked_img = np.concatenate((img,mask_seg,boundary,np.maximum(mask_seg/2,boundary),super_boundary[:,:,None],super_boundary_2[:,:,None]),axis=2)
-        data_patches = extract_patches_2d(stacked_img, (self.patch_size,self.patch_size) ,random_state=1000,max_patches=self.sample).astype(np.float64)
-        #if self.patches.ndim==1:
-        #    self.patches = data_patches.copy().reshape(data_patches.shape[0], -1)
-        #else:
-        #    self.patches = np.concatenate( (self.patches, data_patches.copy().reshape(data_patches.shape[0], -1)),axis=0 )
-        self.patches.append(data_patches.reshape(-1))
-	data_patches_3d = data_patches[:,:,:,:3].copy().reshape(data_patches.shape[0], -1)
-	if self.normalize:
-	    data_patches_3d -= np.mean(data_patches_3d, axis=0)
-	    data_patches_3d /= np.std(data_patches_3d, axis=0)
-        self.patches_3d.append(data_patches_3d.reshape(-1))
-        #if self.patches_3d.ndim==1:
-        #    self.patches_3d=data_patches_3d
-        #else:
-        #    self.patches_3d=np.concatenate( (self.patches_3d, data_patches_3d) , axis=0)
+        return stacked_img.astype(np.uint8)
+
+    def prepare_fit(self,img,mask,mask_seg):
+        if img.sum() in self.img_sig:
+            return 
+        self.img_sig.add(img.sum())
+	img = (img.numpy()[0].transpose(1,2,0)*255).astype(np.uint8)
+        mask = (mask.numpy()[0].transpose(1,2,0)).astype(np.uint8)
+        mask_seg = (mask_seg.numpy()[0].transpose(1,2,0)*255).astype(np.uint8)
+
+        self.images.append((img,mask,mask_seg))
+        self.histograms.append(self.get_hist(img))
+        self.faiss_model=None
 
     def fit(self):
+        #generate the histogram index
+        self.histograms_numpy = np.reshape(self.histograms, newshape=(len(self.histograms), 256*3))
+        self.histogram_model = faiss.IndexFlatL2(256*3)
+        self.histogram_model.add(self.histograms_numpy.astype(np.float32))
+
+
+    def resize(self,img,f):
+        return cv2.resize(img.astype(np.uint8), (0,0), fx=f, fy=f) 
+
+    def make_index(self,image_idxs,use_all=False):
+        print "MAKE INDEX WITH VARIOUS SCALES?? CAN SUPER SCALE? multiply image by 2x, KNN fills in details?"
+        self.patches_3d = []
+        self.patches = []
+        #generate the patches
+        total_patches=0
+        for idx in image_idxs:
+            img,mask,mask_seg = self.images[idx]
+            stacked_img_orig=self.get_stacked(img,mask,mask_seg)
+            h,w=stacked_img_orig.shape[:2]
+            for y in [0.5,1,2]:
+                stacked_img_scaled=self.resize(stacked_img_orig,y) 
+                for x in xrange(4):
+                    stacked_img=random_rotate90_transform1(stacked_img_scaled,x)
+                    #data_patches = extract_patches_2d(stacked_img, (self.patch_size,self.patch_size) ,random_state=1000,max_patches=self.sample).astype(np.float64)
+                    data_patches=None
+                    if use_all:
+                        data_patches = extract_patches_2d(stacked_img, (self.patch_size,self.patch_size) ,random_state=1000).astype(np.float64)
+                    else:
+                        data_patches = extract_patches_2d(stacked_img, (self.patch_size,self.patch_size) ,random_state=1000,max_patches=self.sample).astype(np.float64)
+                    total_patches+=data_patches.shape[0]
+                    self.patches.append(data_patches.reshape(-1))
+                    data_patches_3d = data_patches[:,:,:,:3].copy().reshape(data_patches.shape[0], -1)
+                    if self.normalize:
+                        data_patches_3d -= np.mean(data_patches_3d, axis=0)
+                        data_patches_3d /= np.std(data_patches_3d, axis=0)
+                    self.patches_3d.append(data_patches_3d.reshape(-1))
+    
+        #generate the patch index
         c=self.patch_size*self.patch_size*self.channels
-        self.patches_numpy = np.reshape(self.patches, newshape=(len(self.patches)*self.sample, c))
+        self.patches_numpy = np.reshape(self.patches, newshape=(total_patches, c))
         c=self.patch_size*self.patch_size*3
-        self.patches_3d_numpy = np.reshape(self.patches_3d, newshape=(len(self.patches_3d)*self.sample, c))
-        if self.faiss:
-            self.faiss_model = faiss.IndexFlatL2(self.patches_3d_numpy.shape[1])
-            self.faiss_model.add(self.patches_3d_numpy.astype(np.float32))
-        else:
-            self.model.fit(self.patches_3d_numpy, np.zeros((len(self.patches_3d_numpy))))
+        self.patches_3d_numpy = np.reshape(self.patches_3d, newshape=(total_patches, c))
+
+        model = faiss.IndexFlatL2(self.patches_3d_numpy.shape[1])
+        model.add(self.patches_3d_numpy.astype(np.float32))
+
+        return model
 
     def predict(self,img):
 	img = (img.numpy()[0].transpose(1,2,0)*255).astype(np.uint8)
+
+        patch_model = None
+
+        if self.hist_match:
+            hist = self.get_hist(img)
+            nearest_wds=self.histogram_model.search(hist[None,:].astype(np.float32), self.nn)
+            images_to_use=[]
+            for x in xrange(len(nearest_wds[1][0])):
+	        idx=nearest_wds[1][0][x]
+                if idx==-1:
+                    continue
+                dist=nearest_wds[0][0][x]
+                if dist<self.cutoff:
+                    images_to_use.append(idx)
+            if len(images_to_use)>0:
+                patch_model = self.make_index(images_to_use) #,use_all=True)
+            else:
+                patch_model = self.make_index(nearest_wds[1][0])
+
+        if patch_model==None:
+            if self.faiss_model==None:
+                self.faiss_model = self.make_index(xrange(len(self.images)))
+            patch_model=self.faiss_model
+
         #cv2.imshow('predict img in',img)
         #cv2.waitKey(1)
         #img=img.numpy()[0]
         height,width,channels = img.shape
         img_patches = extract_patches_2d(img, (self.patch_size,self.patch_size)).astype(np.float64)
         img_patches = img_patches.reshape(img_patches.shape[0], -1)
-        mean = np.mean(img_patches, axis=0)
-        std = np.std(img_patches, axis=0)
 	if self.normalize:
+            mean = np.mean(img_patches, axis=0)
+            mean[mean==255]=0
 	    img_patches -= mean
+            std = np.std(img_patches, axis=0)
+            std[std==0]=1
 	    img_patches /= std
         nearest_wds=None
-        if self.faiss:
-            nearest_wds=self.faiss_model.search(img_patches.astype(np.float32), self.n)
-        else:
-            nearest_wds=self.model.kneighbors(img_patches, return_distance=True)
+        nearest_wds=patch_model.search(img_patches.astype(np.float32), self.n)
 	knn_patches=np.array([])
 	for x in xrange(img_patches.shape[0]):
 	    idxs=nearest_wds[1][x]
